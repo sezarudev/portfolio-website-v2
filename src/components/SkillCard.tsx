27e403ce02471884b21e63e0f7cc8412da5dff0c
sezaru-dev <cezarbaguhin20@gmail.com>
2026-06-05 17:23:04 +0800

    refactor(card): update skill card styles

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -5,16 +5,16 @@ import { motion } from 'framer-motion'
 interface SkillCardProps  {
 name : string,
 imgUrl: string
+isCore: boolean
 widthHeight: number
 }
-
-const SkillCard = ({name, imgUrl, widthHeight}:SkillCardProps) => {
+const SkillCard = ({name, imgUrl, isCore, widthHeight}:SkillCardProps) => {
   return (
-    <motion.article whileHover={{ scale: 1.05 }} transition={{ type: "spring", stiffness: 200 }} className='p-4 bg-foreground/5 border border-foreground/10 rounded-lg'>
+    <motion.article whileHover={{ scale: 1.05 }} transition={{ type: "spring", stiffness: 200 }} className={`${isCore ? "bg-primary/5 border border-primary/20" : "bg-surface/40 border border-muted-foreground/15"} p-4 rounded-lg`}>
       <div className='grid grid-cols-1 justify-center items-center'>
         <figure className='flex flex-col items-center justify-center gap-4'>
           <div className='mx-auto'>
-            <Image src={`/${imgUrl}`} alt={`${name} logo`}  width={widthHeight} height={widthHeight} className={["Next", "Github"].includes(name) ? "invert dark:invert-0" : ""} />
+            <Image src={`/${imgUrl}`} alt={`${name} logo`}  width={widthHeight} height={widthHeight} className={["Next.js", "Github"].includes(name) ? "invert dark:invert-0" : ""} />
           </div>
           <figcaption  className='flex items-center justify-center'>
             <h3 className='text-xs md:text-sm'>{name}</h3>
