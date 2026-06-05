dd83e9420d18bd0a1c87a4a536261fa869957204
sezaru-dev <cezarbaguhin20@gmail.com>
2026-06-05 17:29:13 +0800

    refactor(section): update footer and remove themetogglebutton

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,32 +1,28 @@
 import React from "react";
-
 import SocialMediaLinks from "../SocialMediaLinks";
-import ThemeToggle from "../ThemeToggle";
 import { FooterMyName } from "@/constant";
 
 export default function Footer() {
   return (
     <footer 
-      className="relative border-t border-neutral-400/20 px-6 py-10" 
+      className="relative px-6 py-10 bg-background" 
       role="contentinfo"
       aria-label="Website footer"
     >
-      <div className="wrapper grid gap-6 md:grid-cols-3 md:items-center">
-        {/* Left: Copyright */}
-        <p className="text-foreground/80 text-sm text-center md:text-left">
+      <div className="wrapper md:flex gap-6 md:items-center md:justify-between">
+
+        <h2 className='font-bold text-sm text-primary-foreground text-center md:text-left mb-4 md:mb-0'>Sezaru<span className='text-primary'>Dev</span></h2>
+
+        <p className="text-muted-foreground text-sm text-center md:text-left mb-6 md:mb-0">
           © {new Date().getFullYear()} {FooterMyName}. All rights reserved.
         </p>
 
-        {/* Center: Social Icons */}
         <div className="flex justify-center">
           <SocialMediaLinks />
         </div>
 
-        {/* Right: Theme Toggle */}
-        <div className="flex justify-center md:justify-end">
-          <ThemeToggle />
-        </div>
       </div>
+      
     </footer>
   );
 }
